1dd3eb73c505922efce7e278206ce5ad0eb4dc96
Mohit Sharma <skmohit9955@gmail.com>
2026-09-14 10:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function myFunc() {
+	let name = documnet.getElementById("name");
+	let age = Number(document.getELementById("age").value);
+	if(name === "" || age === ""){
+		alert("Please enter valid details.");
+		return;
+	}
+	let result = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age > 18){
+				resolve()
+			}else{
+				reject()
+			}
+		},4000)
+
+})
+	result.then(()=>{
+		alert(`Welcome, ${name} You can vote.`);
+	}).catch(()=>{
+		alert(`Oh sorry ${name} You aren't old enough.`)
+	})
+}
